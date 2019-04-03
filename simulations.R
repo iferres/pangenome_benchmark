668eb3b4926d96f339360ca130e0f7de9ec1959d
iferres <igferres@gmail.com>
2019-04-03 17:44:56 -0300

    fix typo

--- simulations.R
+++ simulations.R
@@ -10,12 +10,13 @@ ref <- "pan_genome_reference.fa"
 
 # Simulation
 dir.create('evo_times')
+dir.create('evo_times/genes_fasta')
 evo_times <- c(1e6, 5e6, 1e7, 5e7, 1e8, 5e8)
-dout <- paste0('evo_times/', sub('[+]','_',as.character(evo_time)))
+dout <- paste0('evo_times/genes_fasta/', sub('[+]','_',as.character(evo_times)))
 
 set.seed(123)
-for (i in seq_along(evo_time)){
-	p <- simpg(ref, ne = evo_time[i], dir_out=dout[i])
+for (i in seq_along(evo_times)){
+	p <- simpg(ref, ne = evo_times[i], dir_out=dout[i])
 	saveRDS(p, file = paste0(dout[i], '/sim_', basename(dout[i]) ,'.RDS'))
 }
 
