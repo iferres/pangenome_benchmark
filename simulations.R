f1961ca3bfea7e53c2f35b1b24614aad570b6974
iferres <igferres@gmail.com>
2019-04-03 12:06:39 -0300

    set.seed before loop

--- simulations.R
+++ simulations.R
@@ -13,8 +13,8 @@ dir.create('evo_times')
 evo_times <- c(1e6, 5e6, 1e7, 5e7, 1e8, 5e8)
 dout <- paste0('evo_times/', sub('[+]','_',as.character(evo_time)))
 
+set.seed(123)
 for (i in seq_along(evo_time)){
-  set.seed(i)
 	p <- simpg(ref, ne = evo_time[i], dir_out=dout[i])
 	saveRDS(p, file = paste0(dout[i], '/sim_', basename(dout[i]) ,'.RDS'))
 }
