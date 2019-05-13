d678ebae6c89837d0538fcc098ea73a2c7f7857d
iferres <igferres@gmail.com>
2019-05-13 15:37:30 -0300

    added replicates

--- simulations.R
+++ simulations.R
@@ -14,10 +14,16 @@ dir.create('evo_times/genes_fasta')
 evo_times <- c(1e6, 5e6, 1e7, 5e7, 1e8, 5e8)
 dout <- paste0('evo_times/genes_fasta/', sub('[+]','_',as.character(evo_times)))
 
-set.seed(123)
+replicates <- 1:5
+seed <- 122
 for (i in seq_along(evo_times)){
-	p <- simpg(ref, ne = evo_times[i], dir_out=dout[i])
-	saveRDS(p, file = paste0(dout[i], '/sim_', basename(dout[i]) ,'.RDS'))
+  for (j in replicates){
+    sseed <- seed + j 
+    set.seed(sseed)
+    dir_out <- paste(dout[i], j, sep = '__')
+    p <- simpg(ref, ne = evo_times[i], dir_out=dir_out)
+    saveRDS(p, file = paste0(dir_out, '/sim_', basename(dir_out) ,'.RDS'))
+  }
 }
 
 
